46c80631621949b0087f015d137f7b01497ea6fc
Libra_mcb <mcb9141018@outlook.com>
2023-10-24 18:08:40 +0800

    add 事务

---
+++ imspring-tx/src/test/java/com/mcb/imspring/tx/ImspringTxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcb.imspring.tx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImspringTxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
