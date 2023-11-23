cedcc1a83eae678b153830b6d360375249fe125c
Libra_mcb <mcb9141018@outlook.com>
2023-11-23 20:30:35 +0800

    事务—流程完成

--- imspring-tx/src/test/java/com/mcb/imspring/tx/test/service/TxService.java
+++ imspring-tx/src/test/java/com/mcb/imspring/tx/test/service/TxService.java
@@ -1,13 +1,21 @@
 package com.mcb.imspring.tx.test.service;
 
+import com.mcb.imspring.core.annotation.Autowired;
 import com.mcb.imspring.core.annotation.Component;
 import com.mcb.imspring.tx.annotation.Transactional;
+import com.mcb.imspring.tx.jdbc.JdbcTemplate;
 
 @Component
 public class TxService {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @Transactional
     public void hello() {
-        System.out.println("hello tx");
+        String sql = "insert into user(name, age, add_time) values('aaa', 18, now())";
+        jdbcTemplate.update(sql);
+//        System.out.println(1/0);
+        System.out.println("数据插入成功");
     }
 }
